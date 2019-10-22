433e585726ee028fe90f27899e02f2a059e5d8ce
moiz <muhammadmoizshaikh@gmail.com>
2019-10-22 20:10:38 +0500

    recipe list component created

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,13 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
+
 export class RecipeListComponent implements OnInit {
-  recipes = [];
-  
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test',
+      'https://images-na.ssl-images-amazon.com/images/I/91CE2CnlnSL._SX466_.jpg'),
+      new Recipe('A Test Recipe', 'This is simply a test',
+      'https://images-na.ssl-images-amazon.com/images/I/91CE2CnlnSL._SX466_.jpg')
+  ];
+  // https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg
+  // https://images-na.ssl-images-amazon.com/images/I/91CE2CnlnSL._SX466_.jpg
   constructor() { }
 
   ngOnInit() {
