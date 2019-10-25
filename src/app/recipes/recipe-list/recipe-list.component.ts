f9437751eeb233ac5a02b961bdde865d21701341
moiz <muhammadmoizshaikh@gmail.com>
2019-10-25 20:25:11 +0500

    eslint configured

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -10,7 +10,7 @@ import { Recipe } from '../recipe.model';
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
     new Recipe('A Test Recipe', 'This is simply a test',
-      'https://images-na.ssl-images-amazon.com/images/I/91CE2CnlnSL._SX466_.jpg'),
+    'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'),
     new Recipe('A Test Recipe', 'This is simply a test',
       'https://images-na.ssl-images-amazon.com/images/I/91CE2CnlnSL._SX466_.jpg')
   ];
